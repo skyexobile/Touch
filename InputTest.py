88b2ac15825de467b62c89a367f7f8ac51ec284f
Angela C <Skye@Angelas-MacBook-Air.local>
2018-08-07 00:49:40 -0500

    made changes to these files

--- InputTest.py
+++ InputTest.py
@@ -3,7 +3,7 @@ import numpy as np
 from time import gmtime, strftime
 import socket
 import tkinter as tk
-
+import csv
 input_serial = serial.Serial('/dev/cu.usbmodem1411')
 input_serial.setBaudrate(115200)
 print("Connected to Sensor")
@@ -13,40 +13,147 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #IP_address = str(sys.argv[1])
 #Port = int(sys.argv[2])server.connect(("localhost", 5000))
 '''
-
+soft_value = 100
+med_value = 500
+hard_value = 1000
 root = tk.Tk()
-label1 = tk.Label(root, text = "Calibration")
 
-
-def getCali():
+def reset():
     input_serial.write(str("0").encode())
-B =  tk.Button(root, text = "Calibrate", command = getCali)
+def set_soft():
+    offset = 0
+    value_list = []
+    max_list = []
+    global soft_value
+    counter = 0
+    while counter <3:
+        value = (input_serial.readline().decode())
+        try:
+            input_value = float(value) + offset
+        except:
+            value = (input_serial.readline().decode())
+            elapsed_time = time.time() - init_time
+            input_value = float(value) + offset
+        if input_value > 50:
+            while input_value >20:
+                value_list.append(input_value)
+            index = value_list.index(max(value_list))
+            max_list.append(value_list[index])
+            max_list.append(value_list[index+1])
+            max_list.append(value_list[index+2])
+            max_list.append(value_list[index+3])
+            counter = counter +1
+        elif input_value < -20:
+            offset = offset - input_value
+    soft_value = sum(max_list)/len(max_list)
+def set_medium():
+    offset = 0
+    value_list = []
+    max_list = []
+    global med_value
+    counter = 0
+    while counter <3:
+        value = (input_serial.readline().decode())
+        try:
+            input_value = float(value) + offset
+        except:
+            value = (input_serial.readline().decode())
+            elapsed_time = time.time() - init_time
+            input_value = float(value) + offset
+        if input_value > 50:
+            while input_value >20:
+                value_list.append(input_value)
+            index = value_list.index(max(value_list))
+            max_list.append(value_list[index])
+            max_list.append(value_list[index+1])
+            max_list.append(value_list[index+2])
+            max_list.append(value_list[index+3])
+            counter = counter +1
+        elif input_value < -20:
+            offset = offset - input_value
+    med_value = sum(max_list)/len(max_list)
+def set_hard():
+    offset = 0
+    value_list = []
+    max_list = []
+    global hard_value
+    counter = 0
+    while counter <3:
+        value = (input_serial.readline().decode())
+        try:
+            input_value = float(value) + offset
+        except:
+            value = (input_serial.readline().decode())
+            elapsed_time = time.time() - init_time
+            input_value = float(value) + offset
+        if input_value > 50:
+            while input_value >20:
+                value_list.append(input_value)
+            index = value_list.index(max(value_list))
+            max_list.append(value_list[index])
+            max_list.append(value_list[index+1])
+            max_list.append(value_list[index+2])
+            max_list.append(value_list[index+3])
+            counter = counter +1
+        elif input_value < -20:
+            offset = offset - input_value
+    hard_value = sum(max_list)/len(max_list)
+def save_settings():
+    global soft_value, med_value, hard_value
+    data = [soft_value, med_value, hard_value]
+    csv_writer(data, "Settings.csv")
+
+def record():
+    init_time = time.time()
+    offset = 0
+    while True:
+        value = (input_serial.readline().decode())
+        elapsed_time = time.time() - init_time
+        try:
+            input_value = float(value) + offset
+        except:
+            value = (input_serial.readline().decode())
+            elapsed_time = time.time() - init_time
+            input_value = float(value) + offset
+        if input_value <-20:
+            offset = offset -input_value
+            try:
+                input_value = float(value) + offset
+            except:
+                value = (input_serial.readline().decode())
+                elapsed_time = time.time() - init_time
+                input_value = float(value) + offset
+        message ='{:.2f}'.format(elapsed_time) + "," + str(input_value)
+        message= message.replace("\n", "")
+        data.append(message)
+        print(message)
+        csv_writer(data, path)
+        root.update()
+B =  tk.Button(root, text = "Reset Sensor", command = reset)
+B2 =  tk.Button(root, text = "Record", command = record)
+B3  =  tk.Button(root, text = "Define Soft", command = set_soft)
+B4  =  tk.Button(root, text = "Define Medium", command = set_medium)
+B5  =  tk.Button(root, text = "Define Hard", command = set_hard)
+B6  =  tk.Button(root, text = "Save Settings", command = save_settings)
 
 B.pack(side = tk.BOTTOM)
+B2.pack(side = tk.BOTTOM)
+B3.pack(side = tk.BOTTOM)
+B4.pack(side = tk.BOTTOM)
+B5.pack(side = tk.BOTTOM)
+B6.pack(side = tk.BOTTOM)
 root.update()
 aquired_flag = False
-previous_read = -5000
 message = "0"
-while True:
-    if not aquired_flag:
-        value = (input_serial.readline().decode())
-        input_value = float(value)
-        if input_value >= previous_read:
-            message = str(input_value)
-            message= message.replace("\n", "")
-            previous_read = input_value
-        else:
-            acquired_flag = True
-            message = "0"
-            while(acquired_flag and input_value >= 30):
-                value = (input_serial.readline().decode())
-                input_value = float(value)
-                input_serial.write(str("0").encode())
-                input_serial.readline().decode()
-                # server.send(message.encode())
-            print('ready')
-            acquired_flag = false
-        #server.send(message.encode())
-    root.update()
+previous_read = -5000
+path = "Input_Values.csv"
+data = []
+def csv_writer(data, path):
+    with open(path, "w", newline = '') as csv_file:
+        writer = csv.writer(csv_file, delimiter = ' ')
+        for value in data:
+            csv_file.write(value + '\n')
+        csv_file.close()
 
-server.close()
+while True:
+    root.update()
--- OutputTest.py
+++ OutputTest.py
@@ -1,8 +1,10 @@
 import serial, datetime, time, re, pickle, os, select, sys
 import tkinter as tk
+from tkinter import filedialog
 import numpy as np
 from time import gmtime, strftime
 import csv
+
 
 print("Connecting to Touch Output")
 output_serial = serial.Serial('/dev/cu.usbmodem1411')
@@ -27,35 +29,56 @@ def load_file():
         #Here is where you should be reading through the file and sending values to output serial
         for row in reader:
             data.append(', '.join(row))
+def sample():
+    output_serial.write(str(4).encode())
+    output_serial.readline().decode()
+    output_serial.write(str(5).encode())
+    output_serial.readline().decode()
+    output_serial.write(str(6).encode())
+    output_serial.readline().decode()
 
 def generate():
+    global acquired_flag, soft_value, medium_value, hard_value
     previous_read = -5000
-    for input_value in data:
-        if( (input_value) >= 100 and (input_value) < 250 and not acquired_flag):
+    for (input_value) in data:
+        print("input value is ", float(input_value))
+        if( float(input_value) >= soft_value-10 and float(input_value) < medium_value and not acquired_flag):
+            print("soft squeeze")
             initial_read = (input_value)
             output_serial.write(str(1).encode())
             #we need to read dummy value after write.
             output_serial.readline().decode()
             acquired_flag = True
-        elif( (input_value) >= 250 and (input_value) <400 ):
+        elif( float(input_value) >= medium_value and float(input_value) <hard_value ):
             initial_read = (input_value)
             output_serial.write(str(2).encode())
             output_serial.readline().decode()
-        elif( (input_value) >= 400):
-            initial_read = (input_value)
+        elif( float(input_value) >= hard_value):
+            initial_read = float(input_value)
             output_serial.write(str(3).encode())
             output_serial.readline().decode()
-        elif(acquired_flag and (previous_read-15)> input_value):
+        elif(acquired_flag and (previous_read-15)> float(input_value)):
             output_serial.write(str(0).encode())
             output_serial.readline().decode()
             acquired_flag = False
-        previous_read = input_value
+        previous_read = float(input_value)
+
 def csv_writer(data, path):
     with open(path, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in data.items():
             writer.writerow(value)
-
+def load_settings():
+    global soft_value, medium_value, hard_value
+    settings = []
+    with open("Settings.csv") as csv_file:
+        reader = csv.reader(csv_file, delimiter = '\n')
+        #Here is where you should be reading through the file and sending values to output serial
+        for row in reader:
+            settings.append(row[0])
+    soft_value = settings[0]
+    medium_value = settings[1]
+    hard_value = settings[2]
 path = "Output_ST.csv"
 myFile = open(path, 'a')
 
@@ -67,11 +90,14 @@ init_flag = False
 B = tk.Button(root, text = "Release", command = release)
 B1 = tk.Button(root, text = "Load File", command = load_file)
 B2 = tk.Button(root, text = "Squeeze Playback", command = generate)
+B3 = tk.Button(root, text = "Sample Squeezes", command = sample)
 B.place(relx=.5, rely=1.5, anchor="center")
 B1.place(relx=.5, rely=1.5, anchor="center")
 B2.place(relx=.5, rely=1.5, anchor="center")
+B3.place(relx=.5, rely=1.5, anchor="center")
 B.pack()
 B1.pack()
 B2.pack()
+B3.pack()
 while True:
     root.update()
